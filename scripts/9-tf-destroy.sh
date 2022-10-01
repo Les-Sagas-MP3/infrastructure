363a3d8862909ea6e4aecda319cff2d4a62462cf
Thomah <5090230+Thomah@users.noreply.github.com>
2022-10-01 19:16:27 +0200

    fix: init terraform before destroying

--- scripts/9-tf-destroy.sh
+++ scripts/9-tf-destroy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+GCP_REGION="europe-west9"
+TF_STATES_BACKEND="les-sagas-mp3-infrastructure"
+
 PROJECT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
 
 ENVIRONMENT_TEMPLATE=$1
 ENVIRONMENT_NAME="${2:-$1}"
+
+# Init Terraform
+cd $PROJECT_PATH/terraform
+terraform init -backend-config="bucket=$TF_STATES_BACKEND" -backend-config="prefix=$ENVIRONMENT_NAME" -reconfigure
 
 # Set local variables
 export TF_VAR_ssh_user=$(whoami)
