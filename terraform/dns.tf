7b44c79fb0b763c385c5db5b405aaaa8fe420e9f
Thomah <5090230+Thomah@users.noreply.github.com>
2022-10-01 17:56:53 +0200

    fix: define A DNS entry different for each environment

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -5,7 +5,7 @@ data "google_dns_managed_zone" "lessagasmp3" {
 
 resource "google_dns_record_set" "dns" {
   project      = var.gcp_project
-  name         = data.google_dns_managed_zone.lessagasmp3.dns_name
+  name         = "${environment_name}.${data.google_dns_managed_zone.lessagasmp3.dns_name}"
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.lessagasmp3.name
