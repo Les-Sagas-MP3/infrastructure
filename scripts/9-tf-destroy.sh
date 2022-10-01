1db62a1b84713c468e19ca04cf9a42354b89faaf
Thomah <5090230+Thomah@users.noreply.github.com>
2022-10-01 17:34:42 +0200

    fix: add missing variables to destroy script

--- scripts/9-tf-destroy.sh
+++ scripts/9-tf-destroy.sh
@@ -5,10 +5,14 @@ PROJECT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/.."
 ENVIRONMENT_TEMPLATE=$1
 ENVIRONMENT_NAME="${2:-$1}"
 
+# Set local variables
+export TF_VAR_ssh_user=$(whoami)
+export TF_VAR_environment_name=$ENVIRONMENT_NAME
+
 # Run plan
 cd $PROJECT_PATH/terraform
-terraform plan -var-file=environments/$ENVIRONMENT_TEMPLATE.tfvars -out .plan/destroy.tfplan -destroy
+terraform plan -var-file=environments/default_versions.tfvars -var-file=environments/$ENVIRONMENT_TEMPLATE.tfvars -out .plan/destroy.tfplan -destroy
 terraform show -no-color -json .plan/destroy.tfplan > .plan/destroy.json
 
 # Run destroy
-terraform destroy -var-file=environments/$ENVIRONMENT_TEMPLATE.tfvars -auto-approve
+terraform destroy -var-file=environments/default_versions.tfvars -var-file=environments/$ENVIRONMENT_TEMPLATE.tfvars -auto-approve
